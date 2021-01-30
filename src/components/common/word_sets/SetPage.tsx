ac10a78c14a45dea68b6f8723b7c8fc63ee62232
MirToykin <miroslav.toykin@gmail.com>
2021-01-30 17:35:00 +0300

    реализовано редактирование слова с помощью новых элементов управления

--- src/components/common/word_sets/SetPage.tsx
+++ src/components/common/word_sets/SetPage.tsx
@@ -6,20 +6,20 @@ import IconButton from "@material-ui/core/IconButton"
 import Icon from "@material-ui/core/Icon"
 import AddToSetForm from "../forms/AddToSetForm"
 import List from "@material-ui/core/List"
-import {useSetStyles} from "../../../assets/useStyles"
+import {useCommonFormStyles, useSetStyles} from "../../../assets/useStyles"
 import TextField from "@material-ui/core/TextField"
 import Set from "./Set"
 import {useDispatch, useSelector} from "react-redux"
 import {
-  deleteWords,
-  GetSetThunkCreatorType, moveWords,
+  deleteWords, editWord,
+  GetSetThunkCreatorType, moveWords, setAddedMeanings,
   setSearchInput,
   SetSearchInputActionType, setSetSize, TGetSet, TMoveAndDeleteWords, TSetSetSizeAction
 } from "../../../redux/actions/wordsActions"
 import LinearProgress from "@material-ui/core/LinearProgress"
 import {OptionsType, SetNameType, WordType} from "../../../types/types";
 import {AppStateType} from "../../../redux/store/configureStore";
-import {Dispatch} from "redux";
+import {AnyAction, Dispatch} from "redux";
 import {ThunkDispatch} from "redux-thunk";
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
@@ -27,6 +27,9 @@ import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import EditIcon from '@material-ui/icons/Edit';
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { useLocation } from 'react-router-dom'
+import ChangeMeaningsForm from "../forms/ChangeMeaningsForm";
+import Dialog from "@material-ui/core/Dialog/Dialog";
+import {stopSubmit} from "redux-form";
 
 type TProps = {
   set: Array<WordType>
@@ -40,19 +43,28 @@ type TProps = {
 
 const SetPage: FC<TProps> = ({set, getSet, pageTitle, uid, addToSet, options}) => {
   const classes = useSetStyles()
+  const classesForm = useCommonFormStyles()
   const [open, setOpen] = useState(false)
+  const [openDialog, setOpenDialog] = useState(false)
   const [selectedIDs, setSelectedIDs] = useState<Array<number>>([])
   const [deltaHeight, setDeltaHeight] = useState(window.pageYOffset ? 90 : 180) // если pageYOffset 0, тогда из высоты контейнера вычитаем 180 px
   const searchInput: string = useSelector((state: AppStateType) => state.words.searchInput)
   const setSize = useSelector((state: AppStateType) => state.words.setSize)
-  const dispatch: Dispatch<SetSearchInputActionType | TSetSetSizeAction> = useDispatch()
+  const dispatchHelpers: Dispatch<SetSearchInputActionType | TSetSetSizeAction> = useDispatch()
+  const dispatch: Dispatch<AnyAction> = useDispatch()
   const thunkDispatchGetSet: ThunkDispatch<AppStateType, unknown, TGetSet> = useDispatch()
   const thunkDispatchMoveAndDelete: ThunkDispatch<AppStateType, unknown, TMoveAndDeleteWords> = useDispatch()
   const isFetching = useSelector((state: AppStateType) => state.app.isFetching)
   const currentRoute: any = useLocation().pathname.slice(1)
   const routes: Array<SetNameType> = ['next', 'current', 'done']
-  const nextRoute = routes.indexOf(currentRoute) === routes.length - 1 ? routes[0] : routes[routes.indexOf(currentRoute) + 1]
-  const prevRoute: SetNameType = routes.indexOf(currentRoute) === 0 ? routes[routes.length - 1] : routes[routes.indexOf(currentRoute) - 1]
+  const currentRouteIndex = routes.indexOf(currentRoute)
+  const nextRoute = currentRouteIndex === routes.length - 1 ? routes[0] : routes[currentRouteIndex + 1]
+  const prevRoute: SetNameType = currentRouteIndex === 0 ? routes[routes.length - 1] : routes[currentRouteIndex - 1]
+  let wordToEdit: WordType | undefined
+
+  wordToEdit = useSelector((state: AppStateType) => {
+    return state.words[routes[currentRouteIndex]].filter(word => word.id === selectedIDs[0])[0]
+  })
 
   const getSetLast = () => thunkDispatchGetSet(getSet(uid, options))
 
@@ -82,7 +94,7 @@ const SetPage: FC<TProps> = ({set, getSet, pageTitle, uid, addToSet, options}) =
   // })
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    dispatch(setSearchInput(e.target.value))
+    dispatchHelpers(setSearchInput(e.target.value))
   }
 
   const handleClose = (additionalActions: Array<() => void>): void => {
@@ -106,86 +118,116 @@ const SetPage: FC<TProps> = ({set, getSet, pageTitle, uid, addToSet, options}) =
 
   const handleMove = (idsArr: Array<number>, setToMove: SetNameType, setToRemoveFrom: SetNameType, options: OptionsType):void => {
     thunkDispatchMoveAndDelete(moveWords(idsArr, setToMove, setToRemoveFrom, options))
+    setSelectedIDs([]);
   }
 
   const handleDelete = (setToRemoveFrom: SetNameType, idsArray: Array<number>, options: OptionsType): void => {
     thunkDispatchMoveAndDelete(deleteWords(setToRemoveFrom, idsArray, options))
+    setSelectedIDs([]);
+  }
+
+  const handleEdit = () => {
+    setOpenDialog(true)
+  }
+
+  const onCloseDialog = () => {
+    setOpenDialog(false)
+    dispatch(setAddedMeanings([]))
+    dispatch(stopSubmit('changeMeaningsForm', {}))
   }
   return (
-    <Grid container justify='center'>
-      <Grid item md={5} sm={7} xs={12}>
-        <Paper style={{marginBottom: '5px'}}>
-          <div className={classes.head}>
-            <Typography variant='h6' color={'textSecondary'}>{pageTitle} - {set.length}</Typography>
-            <div>
-              {deltaHeight === 90 ? // при deltaHeight 90 страница не прокручена, значит показать стрелку вверх, иначе вниз
-                <IconButton onClick={handleScrollUp}>
-                  <Icon>expand_more</Icon>
+    <>
+      wordToEdit && <Dialog open={openDialog} onClose={onCloseDialog}>
+        <Paper className={classesForm.paper}>
+          <Typography variant='h5'
+                      align='center'
+                      color='primary'
+                      className={classesForm.head}
+          >Изменить значения для <Typography variant={'inherit'} color={'textPrimary'}>{wordToEdit && wordToEdit.title}</Typography></Typography>
+          <ChangeMeaningsForm
+            meanings={wordToEdit && wordToEdit.meanings.split('/')}
+            // editWord={editWord}
+            onClose={() => setOpenDialog(false)}
+            id={wordToEdit && wordToEdit.id}
+            options={options}
+          />
+        </Paper>
+      </Dialog>
+      <Grid container justify='center'>
+        <Grid item md={5} sm={7} xs={12}>
+          <Paper style={{marginBottom: '5px'}}>
+            <div className={classes.head}>
+              <Typography variant='h6' color={'textSecondary'}>{pageTitle} - {set.length}</Typography>
+              <div>
+                {deltaHeight === 90 ? // при deltaHeight 90 страница не прокручена, значит показать стрелку вверх, иначе вниз
+                  <IconButton onClick={handleScrollUp}>
+                    <Icon>expand_more</Icon>
+                  </IconButton>
+                  :
+                  <IconButton onClick={handleScrollDown}>
+                    <Icon>expand_less</Icon>
+                  </IconButton>
+                }
+                <IconButton color='primary' style={{padding: '6px', marginRight: '6px'}} size='medium'
+                            onClick={() => setOpen(true)}>
+                  {addToSet && /*<AddIcon fontSize={'large'}/>*/
+                  <Icon style={{fontSize: 40}} color='primary'>add_circle</Icon>}
                 </IconButton>
-              :
-                <IconButton onClick={handleScrollDown}>
-                  <Icon>expand_less</Icon>
-                </IconButton>
-              }
-              <IconButton color='primary' style={{padding: '6px', marginRight: '6px'}} size='medium'
-                          onClick={() => setOpen(true)}>
-                {addToSet && /*<AddIcon fontSize={'large'}/>*/
-                <Icon style={{fontSize: 40}} color='primary'>add_circle</Icon>}
+              </div>
+            </div>
+            {isFetching && !set.length && <LinearProgress color={'secondary'}/>}
+            {addToSet && <AddToSetForm open={open}
+                                       onClose={handleClose}
+                                       addToSet={addToSet}
+                                       uid={uid}
+                                       options={options}
+            />}
+          </Paper>
+          <Paper style={{marginBottom: '5px'}}>
+            <form>
+              <TextField fullWidth={true}
+                         placeholder={'введите строку для поиска'}
+                         inputProps={{
+                           style: {
+                             textAlign: "center"
+                           }
+                         }}
+                         value={searchInput}
+                         onChange={handleInputChange}
+                         disabled={!set.length && !searchInput}
+              />
+            </form>
+          </Paper>
+          <Paper style={{marginBottom: '5px'}}>
+            <div className={classes.multipleActions}>
+              <IconButton onClick={() => handleMove(selectedIDs, prevRoute, currentRoute, options)}>
+                <ArrowBackIcon/>
+              </IconButton>
+              <IconButton onClick={() => handleDelete(currentRoute, selectedIDs, options)}>
+                <DeleteForeverIcon/>
+              </IconButton>
+              <IconButton disabled={selectedIDs.length !== 1} onClick={handleEdit}>
+                <EditIcon/>
+              </IconButton>
+              <IconButton onClick={() => handleMove(selectedIDs, nextRoute, currentRoute, options)}>
+                <ArrowForwardIcon/>
               </IconButton>
             </div>
-          </div>
-          {isFetching && !set.length && <LinearProgress color={'secondary'}/>}
-          {addToSet && <AddToSetForm open={open}
-                                     onClose={handleClose}
-                                     addToSet={addToSet}
-                                     uid={uid}
-                                     options={options}
-          />}
-        </Paper>
-        <Paper style={{marginBottom: '5px'}}>
-          <form>
-            <TextField fullWidth={true}
-                       placeholder={'введите строку для поиска'}
-                       inputProps={{
-                         style: {
-                           textAlign: "center"
-                         }
-                       }}
-                       value={searchInput}
-                       onChange={handleInputChange}
-                       disabled={!set.length && !searchInput}
-            />
-          </form>
-        </Paper>
-        <Paper style={{marginBottom: '5px'}}>
-          <div className={classes.multipleActions}>
-            <IconButton onClick={() => handleMove(selectedIDs, prevRoute, currentRoute, options)}>
-              <ArrowBackIcon/>
-            </IconButton>
-            <IconButton onClick={() => handleDelete(currentRoute, selectedIDs, options)}>
-              <DeleteForeverIcon/>
-            </IconButton>
-            <IconButton disabled={selectedIDs.length !== 1}>
-              <EditIcon/>
-            </IconButton>
-            <IconButton onClick={() => handleMove(selectedIDs, nextRoute, currentRoute, options)}>
-              <ArrowForwardIcon/>
-            </IconButton>
-          </div>
-        </Paper>
-        <Paper>
-          <List id={'words_list'} className={classes.list} style={{maxHeight: `${window.innerHeight - deltaHeight}px`}}>
-            {set.length ? <Set set={set}
-                               pageTitle={pageTitle}
-                               options={options}
-                               wordsCount={setSize}
-                               setSelectedIDs={setSelectedIDs}
-            /> : !set.length && searchInput ?
-              <Typography className={classes.notFound} variant='body1'>Ничего не найдено</Typography> : ''}
-          </List>
-        </Paper>
+          </Paper>
+          <Paper>
+            <List id={'words_list'} className={classes.list} style={{maxHeight: `${window.innerHeight - deltaHeight}px`}}>
+              {set.length ? <Set set={set}
+                                 pageTitle={pageTitle}
+                                 options={options}
+                                 wordsCount={setSize}
+                                 setSelectedIDs={setSelectedIDs}
+              /> : !set.length && searchInput ?
+                <Typography className={classes.notFound} variant='body1'>Ничего не найдено</Typography> : ''}
+            </List>
+          </Paper>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   )
 }
 
